1fe077bdd1f8750fff548b7f9e72a2b52ea708ff
usman <usman.arif@jsys-tech.com>
2026-05-17 19:56:12 +0500

    working on product api

--- ecom/backend/app/Http/Controllers/Api/ProductController.php
+++ ecom/backend/app/Http/Controllers/Api/ProductController.php
@@ -3,12 +3,23 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Product;
+use App\Services\ProductService;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
+    private ProductService $productService;
+
+    public function __construct(ProductService $productService)
+    {
+        $this->productService = $productService;
+    }
+
     public function index()
     {
-        return "this is products api";
+        return response()->json([
+            'service' => $this->productService->getAllProducts()
+        ]);
     }
 }
---
+++ ecom/backend/app/Services/ProductService.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Services;
+
+class ProductService
+{
+
+    public function getAllProducts()
+    {
+        return "this is product service";
+    }
+}
